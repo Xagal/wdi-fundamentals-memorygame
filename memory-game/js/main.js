f2d32aa97aba4f1a648b934cb7460b1b0850ba10
Philip Ryan <philip.ryan15@yahoo.com>
2019-10-05 03:52:37 -0400

    added js and refined the html and css for memory game

---
+++ memory-game/js/main.js
@@ -0,0 +1,7 @@
+console.log("up and running!");
+var card1 = "queen";
+var card2 = "king";
+var card3 = "king";
+var card4 = "queen";
+console.log("User flipped " + card1);
+console.log("User flipped " + card3);
