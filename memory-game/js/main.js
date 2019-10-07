e16aa322b7575d55989ea12ee199547b2006ce4f
Philip Ryan <philip.ryan15@yahoo.com>
2019-10-07 18:48:58 -0400

    updated game logic with objects

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,4 +1,25 @@
-var cards = ["king", "king", "queen", "queen"];
+var cards = [
+    {
+        rank: "queen",
+        suit: "hearts",
+        cardImage: "images/queen-of-hearts.png"
+    },
+    {
+        rank: "queen",
+        suit: "diamonds",
+        cardImage: "images/queen-of-diamonds.png"
+    },
+    {
+        rank: "king",
+        suit: "hearts",
+        cardImage: "images/king-of-hearts.png"
+    },
+    {
+        rank: "king",
+        suit: "diamonds",
+        cardImage: 	"images/king-of-diamonds.png"
+    }
+];
 var cardsInPlay = [];
 
 function checkForMatch() {
@@ -14,8 +35,9 @@ function checkForMatch() {
 }
 
 function flipCard(cardId) {
-    console.log("User flipped " + cards[cardId]);
-    cardsInPlay.push(cards[cardId]);
+    console.log("User flipped " + cards[cardId].rank);
+    console.log(cards[cardId].cardImage);
+    cardsInPlay.push(cards[cardId].rank);
 };
 
 checkForMatch();
@@ -24,4 +46,6 @@ flipCard(0);
 
 flipCard(1)
 
-flipCard(2)
+checkForMatch();
+
+console.log(cardsInPlay);
