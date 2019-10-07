8a5c20ffc7c9db8897bb0f04570b06efb2b86de0
Philip Ryan <philip.ryan15@yahoo.com>
2019-10-07 01:10:48 -0400

    fixed typo in css and created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,7 +1,14 @@
-console.log("up and running!");
-var card1 = "queen";
-var card2 = "king";
-var card3 = "king";
-var card4 = "queen";
-console.log("User flipped " + card1);
-console.log("User flipped " + card3);
+var card = [" king", " king", " queen", " queen"];
+var cardsInPlay = [];
+var cardOne = card[0];
+cardTwo = card[2];
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+console.log("User flipped" + cardsInPlay);
+if (cardsInPlay.length === 2) {
+    if (cardsInPlay [0] === cardsInPlay[1]) {
+        alert("You found a match!")
+    } else {
+        alert("Sorry, try again.")
+    }
+}
