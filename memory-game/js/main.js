052197a03ab8a1448aa233ad0c6fdf60387e66a6
Philip Ryan <philip.ryan15@yahoo.com>
2019-10-08 00:22:53 -0400

    Finished game logic, added scoring and resettable game. Also, card flip displays before alert.

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -20,32 +20,66 @@ var cards = [
         cardImage: 	"images/king-of-diamonds.png"
     }
 ];
-var cardsInPlay = [];
 
-function checkForMatch() {
+var cardsInPlay = [];
+var cardIdInPlay = [];
+var score = 0;
+//not sure what an async function is quite yet but it allowed the sleep function to work
+async function flipCard() {
+    var cardId = this.getAttribute("data-id");
+    console.log("User flipped " + cards[cardId].rank);
+    console.log(cards[cardId].cardImage);
+    cardsInPlay.push(cards[cardId].rank);
+    cardIdInPlay.push(cardId);
+
+
+    this.setAttribute("src", cards[cardId].cardImage);
+
+    //found this function on the web so the card flip displays before the alert.
+    //only solution I could come up with
+    function sleep(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms))
+    };
+      
+    await sleep(300);
+
+    checkMatch()
+};
+
+function createBoard() {
+    for (let i = 0; i < cards.length; i++) {
+        var cardElement = document.createElement("img");
+        cardElement.setAttribute("src", "images/back.png");
+        cardElement.setAttribute("data-id", i);
+        document.getElementById("game-board").appendChild(cardElement);
+        cardElement.addEventListener("click", flipCard);
+    }
+};
+
+function checkMatch() {
     if (cardsInPlay.length === 2) {
         if (cardsInPlay [0] === cardsInPlay[1]) {
-            //alert("You found a match!");
-            console.log("You found a match!");
+            alert("You found a match!");
+            //resetting game
+            for (x in cardIdInPlay) {
+                document.getElementsByTagName("img")[cardIdInPlay[x]].setAttribute("src", "images/back.png");
+            }
+            cardsInPlay = [];
+            cardIdInPlay = [];
+            //scoring
+            score ++;
+            console.log("score: " + score);
+            document.getElementById("score").innerHTML = "Score: " + score;
         } else {
-            //alert("Sorry, try again.");
-            console.log("Sorry, try again.");
+            alert("Sorry, try again.");
+            //resetting game
+            for (x in cardIdInPlay) {
+                document.getElementsByTagName("img")[cardIdInPlay[x]].setAttribute("src", "images/back.png");
+            }
+            cardsInPlay = [];
+            cardIdInPlay = [];
         }
     }
 }
 
-function flipCard(cardId) {
-    console.log("User flipped " + cards[cardId].rank);
-    console.log(cards[cardId].cardImage);
-    cardsInPlay.push(cards[cardId].rank);
-};
-
-checkForMatch();
-
-flipCard(0);
-
-flipCard(1)
-
-checkForMatch();
-
-console.log(cardsInPlay);
+createBoard();
